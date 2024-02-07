36339df29cb1e1f893a77cf5b6ebaf1bee18a882
srinivas.bh <srinivas.bh@agilophiles.com>
2024-02-07 18:55:11 +0530

    CustomHook for data fetching logic

---
+++ src/hooks/useFetchRestaurantDetail.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { RESTAURANT_DETAIL_URI } from "../utils/constants";
+
+const useFetchRestaurantDetail = (id) => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [titleDetails, setTitleDetails] = useState([]);
+  const [offersDetails, setOffersDetails] = useState([]);
+  const [restaurantMenuDetails, setRestaurantMenuDetails] = useState([]);
+  const fetchRestaurantDetails = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get(RESTAURANT_DETAIL_URI + id);
+      setTitleDetails(response.data.data.cards[0].card.card);
+      setOffersDetails(
+        response.data.data.cards[1].card.card.gridElements.infoWithStyle
+      );
+      setRestaurantMenuDetails(
+        response.data.data.cards[2].groupedCard.cardGroupMap.REGULAR.cards.slice(
+          1
+        )
+      );
+      setLoading(false);
+    } catch (error) {
+      setError(error);
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchRestaurantDetails();
+  }, []);
+  return { loading, error, titleDetails, offersDetails, restaurantMenuDetails };
+};
+
+export default useFetchRestaurantDetail;
