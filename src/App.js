6687dbfb8353ed69c94dd09d13ef9c790762b1fd
srinivas.bh <srinivas.bh@agilophiles.com>
2024-02-07 18:49:39 +0530

    Added The Routes to the components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import RestaurantDetail from "./components/RestaurantDetail";
+import Test from "./components/Test";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Body />} />
+        <Route path="/restaurantdetail/:id" element={<RestaurantDetail />} />
+        <Route path="/test" element={<Test />}/>
+      </Routes>
     </div>
   );
 }
