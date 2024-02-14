2f4baf4c4634fa20883f52681fe15e3056d2961d
srinivas.bh <srinivas.bh@agilophiles.com>
2024-02-14 18:27:03 +0530

    Formatting

--- src/components/TitleArrowReusable.js
+++ src/components/TitleArrowReusable.js
@@ -1,37 +1,34 @@
 import React from "react";
-import {
-    LEFT_ARROW_URI,
-    RIGHT_ARROW_URI,
-  } from "../utils/constants";
+import { LEFT_ARROW_URI, RIGHT_ARROW_URI } from "../utils/constants";
 
-const TitleArrowReusable = ({containerRef,title}) => {
-    const moveToLeft = () => {
-      const currentRef = containerRef.current;
-        currentRef.scrollBy({
-            left: -300,
-            behavior: "smooth"
-        })
-    }
-    const moveToRight = () => {
-      const currentRef = containerRef.current;
-        currentRef.scrollBy({
-            left: 300,
-            behavior: "smooth"
-        })
-    }
+const TitleArrowReusable = ({ containerRef, title }) => {
+  const moveToLeft = () => {
+    const currentRef = containerRef.current;
+    currentRef.scrollBy({
+      left: -300,
+      behavior: "smooth",
+    });
+  };
+  const moveToRight = () => {
+    const currentRef = containerRef.current;
+    currentRef.scrollBy({
+      left: 300,
+      behavior: "smooth",
+    });
+  };
   return (
     <main>
-      <div className="flex justify-between items-center">
+      <div className="flex justify-center md:justify-between lg:justify-between xl:justify-between items-center">
         <h1 className="font-bold text-3xl">{title}</h1>
         <div className="flex items-center">
           <img
-            className="w-8 cursor-pointer mx-2"
+            className="w-8 cursor-pointer mx-2 hidden md:block lg:block xl:block"
             src={LEFT_ARROW_URI}
             alt="Left Arrow"
             onClick={moveToLeft}
           />
           <img
-            className="w-8 cursor-pointer mx-2"
+            className="w-8 cursor-pointer mx-2 hidden md:block lg:block xl:block"
             src={RIGHT_ARROW_URI}
             alt="Right Arrow"
             onClick={moveToRight}
